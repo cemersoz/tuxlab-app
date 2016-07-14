719224f2d42637cf324d559d5435e01e8e4b7ba6
cem <cersozz@gmail.com>
2016-07-14 15:01:48 -0400

    fixes

--- client/imports/lab/methods.ts
+++ client/imports/lab/methods.ts
@@ -1,6 +1,9 @@
 Meteor.methods({
   'prepareLab': function(courseId : String ,labId : Number){
     return "here";
+  },
+  'trLab': function(labId: number) : number{
+    return 3;
   }
 });
 
--- server/imports/lab/methods.ts
+++ server/imports/lab/methods.ts
@@ -1,10 +1,19 @@
 declare var Collections : any;
 Meteor.methods({
-  'prepareLab': function(labId : number){
+  /**prepareLab: prepares a labExec object for the current user
+   * takes the id of the lab and a callback as parameter
+   * callback: (err,parseTasks,labExec)
+   */
+  'prepareLab': function(labId : number,callback : any){
      console.log("here");
-     var t = require('../api/labExec.js');
-     t.init(Meteor.userId(),labId,console.log);
-
+     var lab = require('../api/labExec.js');
+     lab.init(Meteor.userId(),labId,function(err,parsed){
+       callback(err,parsed,lab);
+     });
+  },
+  'tyLab': function(labId : number) : number{
+    return 5;
+  }
   }
 });
 
